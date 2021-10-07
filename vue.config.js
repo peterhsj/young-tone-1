427ca33002893af31929cd257e3654ea9f366956
peterhuang <peterhuang@inquartik.com>
2021-10-07 16:09:28 +0800

    feat:新增設定

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  filenameHashing: false,
+  publicPath: './',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  lintOnSave: process.env.NODE_ENV === 'development',
+  productionSourceMap: false,
+}
